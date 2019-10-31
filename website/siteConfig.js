0fcb2bf85c912bc39064f33fcceff39a62a41c0d
William Chen <wchen298@gmail.com>
2019-10-31 01:04:45 -0700

    change siteconfig for gh pages

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,15 +23,15 @@ const users = [
 const siteConfig = {
   title: 'Terra Documentation', // Title for your website.
   tagline: 'Columbus 3',
-  url: 'https://terra.money/', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://ouiliame.github.io', // Your website URL
+  baseUrl: '/terra-docs/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'docs',
-  organizationName: 'terra-project',
+  projectName: 'terra-docs',
+  organizationName: 'ouiliame',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
