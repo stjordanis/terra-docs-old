5eac182e089a1ae4def1a7afa750651915ac1c1b
William Chen <wchen298@gmail.com>
2019-11-03 13:06:34 -0800

    make protocol doc files, add EJS templating, change sidebar styles

--- website/markdownPlugins.js
+++ website/markdownPlugins.js
@@ -1,5 +1,18 @@
 // Customize Markdown
 
+
+const ejs = require("ejs");
+
+// Template engine
+const template = (md) => {
+   md.core.ruler.before("block", "template", (state) => {
+        state.src = ejs.render(state.src, {
+            user: true
+        });
+        return state;
+   });
+};
+
 // Replace inline or links with images
 const plantuml = (md) => {
     require("remarkable-plantuml")(md, { base_path: './static'});
@@ -14,6 +27,7 @@ module.exports = [
             typographer: true
         })
     },
+    template,
     plantuml,
     classy
 ];
